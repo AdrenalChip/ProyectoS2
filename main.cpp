074719729047c26e639dca73808f8b04d236c34d
Jorge Guerrero <jorge.guerrero.0023@gmail.com>
2021-05-20 12:43:37 -0500

    Prueba clase perro

--- main.cpp
+++ main.cpp
@@ -7,3 +7,15 @@ Jorge Guerrero Díaz A01411752
 //CHECAR INDIVIDUAL  Y COLCETIVO 
 // AGREGACION O COMPOSICION
 //necesito un menu con opciones de registro de perro
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <claseperro.h>
+
+using namespace std;
+void main (){
+    int x=1;
+    while (x=1){
+        
+    }
+}
