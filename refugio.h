399adbb7d8f0ae192478e16aa02398fd844e9df8
Jorge Guerrero <jorge.guerrero.0023@gmail.com>
2021-05-25 10:17:52 -0500

    Clase refugio

---
+++ refugio.h
@@ -0,0 +1,20 @@
+/*
+ *Proyecto
+ *Jorge Guerrero Díaz
+ *A01411752
+ *18/05/2021
+*/
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include "claseperro.h"
+
+using namespace std;
+
+class Refugio {
+    private:
+        int capacity=100;
+        
+    public:
+};
